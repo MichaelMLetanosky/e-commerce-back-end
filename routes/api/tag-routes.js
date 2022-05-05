bc516dc43a8093d3f4e83f7a04ab4a13ce5cb714
Michael Letanosky <MichaelMLetanosky@gmail.com>
2022-05-05 15:31:05 -0400

    Added Product Route Functionality

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -71,7 +71,7 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-// delete on tag by its `id` value
+// delete one tag by its `id` value
 router.delete('/:id', async (req, res) => {
   try {
     const tagData = await Tag.destroy({
